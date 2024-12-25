6e381f06da62b4d98939391858b97e2e275659e5
Sandeep Rana <62502757+sandeeprana19@users.noreply.github.com>
2024-12-25 16:39:57 +0530

    cra & tailwindcss setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="text-3xl font-bold text-green-800">
+      Namaste Everyone, let's build netflix-gpt-practice
+    </div>
+  );
+}
+
+export default App;
